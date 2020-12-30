edda3f11ffb41ac51a909433cc4f597dd19a8e24
TSK <rdv260991@gmail.com>
2020-12-30 15:21:00 +0100

    Moved input config to config, added keyboard binding for left trigger, right trigger and L3

--- js/plugins/SRPG_core.js
+++ js/plugins/SRPG_core.js
@@ -96,49 +96,7 @@ var $battleSceneManager = new BattleSceneManager();
     var _srpgDamageDirectionChange = parameters['srpgDamageDirectionChange'] || 'true';
 	var _defaultPlayerSpeed = parameters['defaultPlayerSpeed'] || 4;
 	
-	Input.keyMapper = {
-		9: 'tab',       // tab
-		13: 'menu',       // enter
-		16: 'shift',    // shift
-		17: 'control',  // control
-		18: 'control',  // alt
-		27: 'escape',   // escape
-		32: 'ok',       // space
-		33: 'pageup',   // pageup
-		34: 'pagedown', // pagedown
-		37: 'left',     // left arrow
-		38: 'up',       // up arrow
-		39: 'right',    // right arrow
-		40: 'down',     // down arrow
-		45: 'escape',   // insert
-		81: 'pageup',   // Q
-		87: 'pagedown', // W
-		88: 'escape',   // X
-		90: 'ok',       // Z
-		96: 'escape',   // numpad 0
-		98: 'down',     // numpad 2
-		100: 'left',    // numpad 4
-		102: 'right',   // numpad 6
-		104: 'up',      // numpad 8
-		120: 'debug'    // F9
-	};
 	
-	Input.gamepadMapper = {
-		0: 'ok',        // A
-		1: 'cancel',    // B
-		2: 'shift',     // X
-		3: 'menu',      // Y
-		4: 'pageup',    // LB
-		5: 'pagedown',  // RB
-		12: 'up',       // D-pad up
-		13: 'down',     // D-pad down
-		14: 'left',     // D-pad left
-		15: 'right',    // D-pad right
-		
-		6: "left_trigger",
-		7: "right_trigger",
-		10: "L3"
-	};	
 	
 	Input._isEscapeCompatible = function(keyName) {
 		return keyName === 'cancel';
