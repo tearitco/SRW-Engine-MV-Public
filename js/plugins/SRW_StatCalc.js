ff2030b61a0f7e3b9445d4c3adec68ca7281524a
tshadowknight <tshadowknight@gmail.com>
2021-05-03 10:19:44 +0200

    Updated transform on destruction to also apply deploy actions

--- js/plugins/SRW_StatCalc.js
+++ js/plugins/SRW_StatCalc.js
@@ -1386,6 +1386,12 @@ StatCalc.prototype.transformOnDestruction = function(actor, force){
 	if(this.isActorSRWInitialized(actor) && actor.isActor()){		
 		var transformIntoId = actor.SRWStats.mech.destroyTransformInto;
 		var targetActorId = actor.SRWStats.mech.destroyTransformedActor;
+		
+		actor.isSubPilot = false;
+		actor.SRWStats.mech = this.getMechDataById(transformIntoId, true);
+		this.calculateSRWMechStats(actor.SRWStats.mech);
+		this.applyDeployActions(actor.SRWStats.pilot.id, actor.SRWStats.mech.id);
+		
 		if(targetActorId != null){
 			var targetActor = $gameActors.actor(targetActorId);
 			if(targetActor.actorId() != actor.actorId()){
@@ -1399,9 +1405,7 @@ StatCalc.prototype.transformOnDestruction = function(actor, force){
 				}
 			}
 		}		
-		actor.isSubPilot = false;
-		actor.SRWStats.mech = this.getMechDataById(transformIntoId, true);
-		this.calculateSRWMechStats(actor.SRWStats.mech);					
+							
 		actor.initImages(actor.SRWStats.mech.classData.meta.srpgOverworld.split(","));
 		actor.event.refreshImage();							
 	}
